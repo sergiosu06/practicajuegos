41b188b7a63c3a9b7211e7aebbeb9258def46a56
Sergio <sucapucasergio2.com>
2023-10-30 12:20:41 -0400

    subiendo emailcontrolador

---
+++ VideojuegosSergio/Contexto/Aplicacion_contexto.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProyectoVideoJuegos.Models;
+using VideojuegosSergio.Models;
+
+namespace VideojuegosSergio.Contexto
+{
+    public class Aplicacion_contexto : DbContext
+    {
+        public Aplicacion_contexto
+            (DbContextOptions<Aplicacion_contexto> options)
+            : base(options) { }
+
+        public DbSet<Email> Email { 
+            get; set; 
+        }
+        public DbSet<Usuarios> Usuarios { 
+            get; set; 
+        }
+        public DbSet<Video_juegos> Video_juegos { 
+            get; set; 
+        }
+
+    }
+}
--- VideojuegosSergio/Program.cs
+++ VideojuegosSergio/Program.cs
@@ -1,15 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using VideojuegosSergio.Contexto;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers();https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddDbContext<Aplicacion_contexto>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -17,9 +19,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
